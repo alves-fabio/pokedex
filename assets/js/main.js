bd1c614f4685cc58c489589d2043becae8a9f51d
fabio.alves <fabio.alves@gmail.com>
2023-09-20 23:47:35 -0300

    Create pagination and limit  to return only 1st gen pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const limit =5
+
+const maxRecords = 151
+const limit = 10
 let offset = 0
 
 
@@ -27,5 +29,16 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () =>{
     offset += limit
-    loadPokemonItens(offset, limit)
+
+    const qtdRecordsWithNextPage = offset + limit
+
+    if (qtdRecordsWithNextPage >= maxRecords) {
+        const newLimit =  maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+
 })
